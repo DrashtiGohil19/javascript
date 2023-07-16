394bc9e8be92bf822e48bdc5373a572587ee273f
DrashtiGohil19 <gohildrashti636@gmail.com>
2023-07-16 12:44:01 +0530

    javascript project

---
+++ Restaurant/JS/script.js
@@ -0,0 +1,224 @@
+window.onload=function()
+{
+    a=0+' $',b=0+' $',c=0+' $';
+    document.getElementById('bill').value=a;
+    document.getElementById('gst').value=b;
+    document.getElementById('netbill').value=c;
+}
+amt=0;
+function netamountit1() 
+{
+   a=document.getElementById('it1').checked;
+   if(a==true)
+   {
+        amt=amt+50;
+        var gst=amt*18/100,totalbill=amt+gst;
+   }
+   else
+    {
+        amt=amt-50;
+        var gst=amt*18/100,totalbill=amt+gst;
+    }
+    a=amt+' $',b=gst+' $',c=totalbill+' $';
+    document.getElementById('bill').value=a;
+    document.getElementById('gst').value=b;
+    document.getElementById('netbill').value=c;
+}
+function netamountit2() 
+{
+   a=document.getElementById('it2').checked;
+   if(a==true)
+   {
+        amt=amt+80;
+        var gst=amt*18/100,totalbill=amt+gst;
+   }
+   else
+    {
+        amt=amt-80;
+        var gst=amt*18/100,totalbill=amt+gst;
+    }
+    a=amt+' $',b=gst+' $',c=totalbill+' $';
+    document.getElementById('bill').value=a;
+    document.getElementById('gst').value=b;
+    document.getElementById('netbill').value=c;
+}
+function netamountit3() 
+{
+   a=document.getElementById('it3').checked;
+   if(a==true)
+   {
+        amt=amt+110;
+        var gst=amt*18/100,totalbill=amt+gst;
+   }
+   else
+    {
+        amt=amt-110;
+        var gst=amt*18/100,totalbill=amt+gst;
+    }
+    a=amt+' $',b=gst+' $',c=totalbill+' $';
+    document.getElementById('bill').value=a;
+    document.getElementById('gst').value=b;
+    document.getElementById('netbill').value=c;
+}
+function netamountit4() 
+{
+   a=document.getElementById('it4').checked;
+   if(a==true)
+   {
+        amt=amt+140;
+        var gst=amt*18/100,totalbill=amt+gst;
+   }
+   else
+    {
+        amt=amt-140;
+        var gst=amt*18/100,totalbill=amt+gst;
+    }
+    a=amt+' $',b=gst+' $',c=totalbill+' $';
+    document.getElementById('bill').value=a;
+    document.getElementById('gst').value=b;
+    document.getElementById('netbill').value=c;
+}
+function netamountit5() 
+{
+   a=document.getElementById('it5').checked;
+   if(a==true)
+   {
+        amt=amt+170;
+        var gst=amt*18/100,totalbill=amt+gst;
+   }
+   else
+    {
+        amt=amt-170;
+        var gst=amt*18/100,totalbill=amt+gst;
+    }
+    a=amt+' $',b=gst+' $',c=totalbill+' $';
+    document.getElementById('bill').value=a;
+    document.getElementById('gst').value=b;
+    document.getElementById('netbill').value=c;
+}
+function netamountit6() 
+{
+   a=document.getElementById('it6').checked;
+   if(a==true)
+   {
+        amt=amt+200;
+        var gst=amt*18/100,totalbill=amt+gst;
+   }
+   else
+    {
+        amt=amt-200;
+        var gst=amt*18/100,totalbill=amt+gst;
+    }
+    a=amt+' $',b=gst+' $',c=totalbill+' $';
+    document.getElementById('bill').value=a;
+    document.getElementById('gst').value=b;
+    document.getElementById('netbill').value=c;
+}
+function netamountit7() 
+{
+   a=document.getElementById('it7').checked;
+   if(a==true)
+   {
+        amt=amt+230;
+        var gst=amt*18/100,totalbill=amt+gst;
+   }
+   else
+    {
+        amt=amt-230;
+        var gst=amt*18/100,totalbill=amt+gst;
+    }
+    a=amt+' $',b=gst+' $',c=totalbill+' $';
+    document.getElementById('bill').value=a;
+    document.getElementById('gst').value=b;
+    document.getElementById('netbill').value=c;
+}
+function netamountit8() 
+{
+   a=document.getElementById('it8').checked;
+   if(a==true)
+   {
+        amt=amt+260;
+        var gst=amt*18/100,totalbill=amt+gst;
+   }
+   else
+    {
+        amt=amt-260;
+        var gst=amt*18/100,totalbill=amt+gst;
+    }
+    a=amt+' $',b=gst+' $',c=totalbill+' $';
+    document.getElementById('bill').value=a;
+    document.getElementById('gst').value=b;
+    document.getElementById('netbill').value=c;
+}
+function netamountit9() 
+{
+   a=document.getElementById('it9').checked;
+   if(a==true)
+   {
+        amt=amt+290;
+        var gst=amt*18/100,totalbill=amt+gst;
+   }
+   else
+    {
+        amt=amt-290;
+        var gst=amt*18/100,totalbill=amt+gst;
+    }
+    a=amt+' $',b=gst+' $',c=totalbill+' $';
+    document.getElementById('bill').value=a;
+    document.getElementById('gst').value=b;
+    document.getElementById('netbill').value=c;
+}
+function netamountit10() 
+{
+   a=document.getElementById('it10').checked;
+   if(a==true)
+   {
+        amt=amt+320;
+        var gst=amt*18/100,totalbill=amt+gst;
+   }
+   else
+    {
+        amt=amt-320;
+        var gst=amt*18/100,totalbill=amt+gst;
+    }
+    a=amt+' $',b=gst+' $',c=totalbill+' $';
+    document.getElementById('bill').value=a;
+    document.getElementById('gst').value=b;
+    document.getElementById('netbill').value=c;
+}
+function netamountit11() 
+{
+   a=document.getElementById('it11').checked;
+   if(a==true)
+   {
+        amt=amt+350;
+        var gst=amt*18/100,totalbill=amt+gst;
+   }
+   else
+    {
+        amt=amt-350;
+        var gst=amt*18/100,totalbill=amt+gst;
+    }
+    a=amt+' $',b=gst+' $',c=totalbill+' $';
+    document.getElementById('bill').value=a;
+    document.getElementById('gst').value=b;
+    document.getElementById('netbill').value=c;
+}
+function netamountit12() 
+{
+   a=document.getElementById('it12').checked;
+   if(a==true)
+   {
+        amt=amt+380;
+        var gst=amt*18/100,totalbill=amt+gst;
+   }
+   else
+    {
+        amt=amt-380;
+        var gst=amt*18/100,totalbill=amt+gst;
+    }
+    a=amt+' $',b=gst+' $',c=totalbill+' $';
+    document.getElementById('bill').value=a;
+    document.getElementById('gst').value=b;
+    document.getElementById('netbill').value=c;
+}
